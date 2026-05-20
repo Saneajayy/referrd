babc9ac32caf75ffbec4ee46f0d28934bf91d778
saneajayy <ajaykumarrj302@gmail.com>
2026-05-20 22:39:13 +0530

    Clear repository: delete every file and clear the folder to start from scratch

--- server/routes/companies.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getCompanies, createCompany } = require('../controllers/companyController');
-const { verifyAdminToken } = require('../middleware/authMiddleware');
-
-router.get('/', getCompanies);
-router.post('/', verifyAdminToken, createCompany);
-
-module.exports = router;
--- server/routes/match.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { matchResume } = require('../controllers/matchController');
-const { verifyUserToken } = require('../middleware/authMiddleware');
-
-router.post('/', verifyUserToken, matchResume);
-
-module.exports = router;
